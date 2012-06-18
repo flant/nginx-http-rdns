b995a328a496e98dfdd881c9b433f953f2bd38eb
Timofey.Kirillov <distorhead@gmail.com>
2012-06-18 13:07:36 +0400

    ++

--- ngx_http_rdns_module.c
+++ ngx_http_rdns_module.c
@@ -118,8 +118,8 @@ static ngx_http_module_t  ngx_http_rdns_module_ctx = {
 
 ngx_module_t  ngx_http_rdns_module = {
     NGX_MODULE_V1,
-    &ngx_http_rdns_module_ctx, /* module context */
-    ngx_http_rdns_commands,    /* module directives */
+    &ngx_http_rdns_module_ctx,        /* module context */
+    ngx_http_rdns_commands,           /* module directives */
     NGX_HTTP_MODULE,                  /* module type */
     NULL,                             /* init master */
     NULL,                             /* init module */
